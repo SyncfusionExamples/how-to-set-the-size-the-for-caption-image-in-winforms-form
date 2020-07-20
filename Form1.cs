cba3afb2dc945b804612093aeea071dd5941afd0
vijayarasan <58214164+vijayarasan@users.noreply.github.com>
2020-07-20 18:53:58 +0530

    How to set the size the for caption image in WinForms Form(SfForm)?
    How to set the size the for caption image in WinForms Form(SfForm)?

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using Syncfusion.Windows.Forms;
+using Syncfusion.WinForms.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : SfForm
+    {
+        public Form1()
+        {
+            InitializeComponent();           
+
+            ImageList list = new ImageList();
+            list.Images.Add(SystemIcons.Application.ToBitmap());
+
+            //ImageSize set as 20
+            /*list.ImageSize = new Size(20, 20)*/;
+
+            //Image size set as 40
+            list.ImageSize = new Size(40, 40);
+
+            this.Style.TitleBar.CaptionImage = list.Images[0];
+            this.Style.TitleBar.CaptionImageLocation = new Point(40, 4);
+        }
+    }
+}
